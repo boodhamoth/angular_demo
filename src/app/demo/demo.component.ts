6442cb314f73bee4fd2e7094d0eed18fb85deceb
seetharaman gr <support@triodesk.com>
2018-01-10 23:12:33 +0530

    changes

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnInit {
+export class DemoComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit(){
+    $('#dater').bootstrapMaterialDatePicker({ weekStart : 0, time: false });
   }
 
 }
